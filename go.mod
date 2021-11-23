f702db58646eba1c51eb22cd448378e680a2b45d
Alex <alex@linux.fritz.box>
2021-11-23 17:53:11 +0100

    lul

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module consoleEngine
 
 go 1.15
 
-require github.com/fatih/color v1.12.0
+require (
+	github.com/fatih/color v1.12.0
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871
+)
--- main.go
+++ main.go
@@ -25,39 +25,78 @@ func main() {
 		return
 	}
 
-	//ticker := time.NewTicker(33 * time.Millisecond)
-	var b bytes.Buffer
+	/*	//ticker := time.NewTicker(33 * time.Millisecond)
+		var b bytes.Buffer
 
-	frameChan := make(chan int)
-	paintChan := make(chan int)
+		frameChan := make(chan int)
+		paintChan := make(chan int)
 
-	go func(gif *gif.GIF,b io.Writer,frameChan,paintChan chan int) {
-			for _,image := range g.Image {
-				select {
-				case i := <-paintChan:
-					GUI.PaintGif(image, b)
-					frameChan <- i
-				}
+		func(gif *gif.GIF, b io.Writer, frameChan, paintChan chan int) {
+			for _, image := range g.Image {
+				//	select {
+				//	case i := <-paintChan:
+				GUI.PaintGif(image, b)
+				//		frameChan <- i
+				io.Copy(os.Stdout, b)
+				cmd := exec.Command("clear")
+				cmd.Stdout = os.Stdout
+				cmd.Run()
+
+				//	}
 			}
-	}(g,&b,frameChan,paintChan)
+		}(g, &b, frameChan, paintChan)
+		/*
+		   	paintChan <- 0
+		   	ticker := time.NewTicker(33 * time.Millisecond)
+		   	for {
+		   		var copy bytes.Buffer
+		   		select {
+		   		case i := <-frameChan:
+		   			copy = b
+		   			i += 1
 
-	paintChan <- 1
-	ticker := time.NewTicker(33*time.Millisecond)
-	for  {
-		var copy bytes.Buffer
-		select {
-			case i:=<-frameChan:
-				copy = b
-				i+=1
-				paintChan <- i
+		   		case <-ticker.C:
+		   			//GUI.PaintGif(image, &b)
+		   			io.Copy(os.Stdout, &copy)
+		   			paintChan <- 0
+		   			//	time.Sleep(1*time.Second)
+		   		}
+		   		cmd := exec.Command("clear")
+		   		cmd.Stdout = os.Stdout
+		   		cmd.Run()
+		   	}
+		   }
 
-			case <-ticker.C:
-				//GUI.PaintGif(image, &b)
-				io.Copy(os.Stdout, &copy)
-			//	time.Sleep(1*time.Second)
-			}
-		//	<-ticker.C
-		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+	*/
+
+	bufferChan := make(chan bytes.Buffer)
+	framePaintedChan := make(chan int)
+
+	go WriteFrame(g, bufferChan, framePaintedChan)
+	go ScreenClearer(bufferChan)
+
+	//	var b bytes.Buffer
+	for { //_, image := range g.Image {
+		b := <-bufferChan
+		//GUI.PaintGif(image, &b)
+		io.Copy(os.Stdout, &b)
+	}
+}
+
+func WriteFrame(gif *gif.GIF, bufferChan chan bytes.Buffer, framePaintedChan chan int) {
+	ticker := time.NewTicker(33 * time.Millisecond)
+	for _, image := range gif.Image {
+		<-ticker.C
+		var b bytes.Buffer
+		GUI.PaintGif(image, &b)
+		bufferChan <- b
+	}
+}
+
+func ScreenClearer(bufferChan chan bytes.Buffer) {
+	for {
+		<-bufferChan
+		cmd := exec.Command("clear")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 	}
